256184e1281011af79d29c4d5ac92d44f0c0ba92
rajesh23-drg <rmanjunath@teamdrg.com>
2019-03-09 09:30:03 +0530

    registering flight-schedule service to eureka

--- flight-schedule/src/main/java/com/course/ms/flightSchedule/FlightScheduleApplication.java
+++ flight-schedule/src/main/java/com/course/ms/flightSchedule/FlightScheduleApplication.java
@@ -2,8 +2,10 @@ package com.course.ms.flightSchedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class FlightScheduleApplication {
 
 	public static void main(String[] args) {
