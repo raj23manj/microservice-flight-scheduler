516c959289a2d7b5f68c9789a54acdfaba618956
rajesh23-drg <rmanjunath@teamdrg.com>
2019-03-10 07:31:51 +0530

    added zuul pre filters

---
+++ zuul-service-gateway/src/main/java/com/course/ms/zuulservicegateway/filters/AuthenticationFilter.java
@@ -0,0 +1,48 @@
+package com.course.ms.zuulservicegateway.filters;
+
+import org.springframework.cloud.netflix.zuul.filters.support.FilterConstants;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+public class AuthenticationFilter extends ZuulFilter {
+
+	
+	// this filter needs to be executed after the pre-decoration filter, the below one
+	
+	@Override
+	public Object run() throws ZuulException {
+
+		RequestContext ctx = RequestContext.getCurrentContext();
+		System.out.println("Entered into pre filter ->>>>" + ctx.getRequest().getRequestURI());
+		// this service-id will be only set after pre-decoration filter
+		Object serviceId = ctx.get("serviceId");
+		if (serviceId != null && FilterUtils.FLIGHT_SCHEDULE_SERVICE_NAME.equals(serviceId.toString())) {
+			String token = ctx.getRequest().getParameter(FilterUtils.FLIGHT_SCHEDULE_AUTH_TOKEN_KEY);
+			if (!FilterUtils.FLIGHT_SCHEDULE_AUTH_TOKEN_VALUE.equals(token)) {
+				ctx.setResponseStatusCode(401);
+				ctx.setResponseBody("The request is not authenticated");
+				ctx.getResponse().setContentType("application/json");
+				ctx.setSendZuulResponse(false);
+			}
+		}
+		return null;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public int filterOrder() {
+		return FilterUtils.AUTHENTICATION_FILTER_ORDER;
+	}
+
+	@Override
+	public String filterType() {
+		return FilterConstants.PRE_TYPE;
+	}
+
+}
