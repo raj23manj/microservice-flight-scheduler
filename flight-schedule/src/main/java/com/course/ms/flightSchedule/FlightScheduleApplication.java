48d608f75b9577143d90e6e2e9eb5c06cccce949
rajesh23-drg <rmanjunath@teamdrg.com>
2019-03-10 11:40:31 +0530

    added hystrix using propery file setting with command key and method name

--- flight-schedule/src/main/java/com/course/ms/flightSchedule/FlightScheduleApplication.java
+++ flight-schedule/src/main/java/com/course/ms/flightSchedule/FlightScheduleApplication.java
@@ -2,10 +2,12 @@ package com.course.ms.flightSchedule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class FlightScheduleApplication {
 
 	public static void main(String[] args) {
--- flight-schedule/src/main/java/com/course/ms/flightSchedule/services/FlightScheduleServiceImpl.java
+++ flight-schedule/src/main/java/com/course/ms/flightSchedule/services/FlightScheduleServiceImpl.java
@@ -1,6 +1,7 @@
 package com.course.ms.flightSchedule.services;
 
 import java.util.List;
+import java.util.Random;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.util.CollectionUtils;
 
 import com.course.ms.flightSchedule.dao.FlightScheduleRepository;
 import com.course.ms.flightSchedule.models.Flight;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @Service("flightScheduleService")
 public class FlightScheduleServiceImpl implements FlightScheduleService{
@@ -20,13 +22,15 @@ public class FlightScheduleServiceImpl implements FlightScheduleService{
 	
 	@Autowired
 	private FlightScheduleRepository flightRepository;
-
+    
+	@HystrixCommand(commandKey="getFlightsKey")
 	@Override
 	public List<Flight> getFLights(String from, String to) {
 		Flight filterFlight = new Flight();
 		filterFlight.setFlightFrom(from);
 		filterFlight.setFlightTo(to);
 		// https://stackoverflow.com/questions/27626825/spring-data-jpa-query-by-example
+		sleepRandomly();
 		Example<Flight> flightFilterExample = Example.of(filterFlight);
 		List<Flight> flightList = flightRepository.findAll(flightFilterExample);
 		
@@ -36,6 +40,21 @@ public class FlightScheduleServiceImpl implements FlightScheduleService{
 		}
 		
 		return flightList;
+	}
+	
+	public void sleepRandomly() {
+		Random rand = new Random();
+		int randomNum = rand.nextInt(3) + 1;
+		if(randomNum == 3) {
+			System.out.println("It is a chance for demonstrating Hystrix action");
+			try {
+				System.out.println("Start sleeping...." + System.currentTimeMillis());
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				System.out.println("Hystrix thread interupted...." + System.currentTimeMillis());
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
